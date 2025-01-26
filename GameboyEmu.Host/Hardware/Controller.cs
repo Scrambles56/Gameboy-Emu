36411ccda0ba61e38c6b893934ce0d952c82517f
Matt Madigan <mmhawk576@gmail.com>
2025-01-26 14:29:20 +1300

    Starting input handling

---
+++ GameboyEmu.Host/Hardware/Controller.cs
@@ -0,0 +1,39 @@
+﻿using Raylib_cs;
+
+namespace GameboyEmu.Windowing;
+
+public class Controller
+{
+    public Dictionary<GbButton, KeyboardKey> ButtonMappings { get; } = new()
+    {
+        { GbButton.Up, KeyboardKey.Up },
+        { GbButton.Down, KeyboardKey.Down },
+        { GbButton.Left, KeyboardKey.Left },
+        { GbButton.Right, KeyboardKey.Right },
+        { GbButton.A, KeyboardKey.Z },
+        { GbButton.B, KeyboardKey.X },
+        { GbButton.Start, KeyboardKey.Enter },
+        { GbButton.Select, KeyboardKey.RightShift }
+    };
+    
+    private List<GbButton> GetPressedButtons()
+    {
+        return ButtonMappings.Keys
+            .Where(button => Raylib.IsKeyDown(ButtonMappings[button]))
+            .ToList();
+    }
+    
+    public bool IsKeyDown(GbButton button) => Raylib.IsKeyDown(ButtonMappings[button]);
+}
+
+public enum GbButton
+{
+    Up,
+    Down,
+    Left,
+    Right,
+    A,
+    B,
+    Start,
+    Select
+}
